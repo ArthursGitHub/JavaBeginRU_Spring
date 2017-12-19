57e18b55cceb809271c6d02c3c11860c1f18e3e3
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-12-19 03:39:31 +0300

    0

--- Lesson_10/src/main/java/ru/javabegin/training/spring/main/Start.java
+++ Lesson_10/src/main/java/ru/javabegin/training/spring/main/Start.java
@@ -6,7 +6,7 @@ import ru.javabegin.training.spring.impls.robot.ModelT1000;
 
 public class Start {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"context.xml"});
         // ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
         // System.out.println(t1000.getHead());
         // t1000 = (ModelT1000) context.getBean("t1000");
@@ -15,8 +15,8 @@ public class Start {
         // System.out.println(t1000.getHead());
 
          ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
-         System.out.println(t1000.getHead());
+         System.out.println(t1000);
          t1000 = (ModelT1000) context.getBean("t1000");
-         System.out.println(t1000.getHead());
+         System.out.println(t1000);
     }
 }
