77747769ca91b76676f70dc08aeda90f95697c8a
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-12-19 19:43:53 +0300

    0

--- Lesson_18/src/main/java/ru/javabegin/training/spring/main/Start.java
+++ Lesson_18/src/main/java/ru/javabegin/training/spring/main/Start.java
@@ -2,14 +2,14 @@ package ru.javabegin.training.spring.main;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import ru.javabegin.training.spring.impls.robot.ModelT1000;
+import ru.javabegin.training.spring.impls.pool.T1000Pool;
 
 public class Start {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("all_context.xml");
 
-        ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
-        t1000.action();
+        T1000Pool t1000GoldenPool = (T1000Pool) context.getBean("t1000Pool");
+
+        t1000GoldenPool.beginShow();
     }
 }
