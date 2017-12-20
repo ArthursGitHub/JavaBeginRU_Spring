e83497528f3f471163b2b2fdf876d9561d82560d
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-12-20 19:51:04 +0300

    0

---
+++ Lesson_34/src/main/java/ru/javabegin/training/spring/main/Start.java
@@ -0,0 +1,23 @@
+package ru.javabegin.training.spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ru.javabegin.training.spring.dao.interfaces.MP3Dao;
+import ru.javabegin.training.spring.dao.objects.MP3;
+
+public class Start {
+    public static void main(String[] args) {
+        MP3 mp3 = new MP3();
+        mp3.setName("My Song");
+        mp3.setAuthor("Arthur");
+
+        // new SQLiteDAO().insertWithJDBC(mp3);
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        MP3Dao sqLiteDAO = (MP3Dao) context.getBean("sqliteDAO");
+
+        sqLiteDAO.insert(mp3);
+        System.out.println(sqLiteDAO.getMP3ListByAuthor("Arthur").size());
+    }
+}
